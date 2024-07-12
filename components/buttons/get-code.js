5456e47f922b675a6c8816ce7e75dc8dafa3af7e
Simon <yash1441@yahoo.com>
2024-07-12 13:30:16 +0530

    Added activation code logic

---
+++ components/buttons/get-code.js
@@ -0,0 +1,40 @@
+const { bold, inlineCode } = require("discord.js");
+const lark = require("../utils/lark.js");
+require("dotenv").config();
+
+module.exports = {
+	cooldown: 10,
+	data: {
+		name: "get-code",
+	},
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+		const discordId = interaction.user.id;
+
+		const response = await lark.listRecords(
+			process.env.FEEDBACK_POOL_BASE,
+			process.env.CODES_TABLE,
+			{ filter: 'CurrentValue.[Discord ID] = "' + discordId + '"' }
+		);
+
+		if (!response)
+			return await interaction.editReply({
+				content:
+					"You aren't registered. Please use the " +
+					bold("Register") +
+					" button to register.",
+			});
+
+		if (response.total) {
+			const code = response.items[0]["Activation Code"];
+			if (!code)
+				return await interaction.editReply({
+					content:
+						"You currently have no code assigned to you or the code distribution hasn't begun. Please try again later.",
+				});
+			return await interaction.editReply({
+				content: "Your activation code is " + inlineCode(code),
+			});
+		}
+	},
+};
---
+++ components/buttons/register-alpha.js
@@ -0,0 +1,51 @@
+const { bold } = require("discord.js");
+const lark = require("../utils/lark.js");
+require("dotenv").config();
+
+module.exports = {
+	cooldown: 10,
+	data: {
+		name: "register-alpha",
+	},
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+		const discordId = interaction.user.id;
+
+		const response = await lark.listRecords(
+			process.env.FEEDBACK_POOL_BASE,
+			process.env.CODES_TABLE,
+			{ filter: 'CurrentValue.[Discord ID] = "' + discordId + '"' }
+		);
+
+		if (!response)
+			return await interaction.editReply({
+				content: "An error has occured. Please try again later.",
+			});
+
+		if (response.total)
+			return await interaction.editReply({
+				content:
+					"You are already registered! Please use the " +
+					bold("Get Code") +
+					"button once the code distribution has started to get your code.",
+			});
+
+		const success = await lark.createRecord(
+			process.env.FEEDBACK_POOL_BASE,
+			process.env.CODES_TABLE,
+			{ fields: { "Discord ID": discordId } }
+		);
+
+		if (success)
+			return await interaction.editReply({
+				content:
+					"You have been registered! Please use the " +
+					bold("Get Code") +
+					"button once the code distribution has started to get your code.",
+			});
+		else
+			return await interaction.editReply({
+				content: "An error has occured. Please try again later.",
+			});
+	},
+};
