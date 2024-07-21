77da52381779f4d2863cbf27730a082dba834867
Simon <yash1441@yahoo.com>
2024-07-22 01:50:21 +0530

    Added register options

--- components/buttons/register-alpha.js
+++ components/buttons/register-alpha.js
@@ -1,4 +1,9 @@
-const { bold } = require("discord.js");
+const {
+	bold,
+	ButtonBuilder,
+	ButtonStyle,
+	ComponentType,
+} = require("discord.js");
 const lark = require("../../utils/lark");
 require("dotenv").config();
 
@@ -9,12 +14,17 @@ module.exports = {
 	},
 	async execute(interaction) {
 		await interaction.deferReply({ ephemeral: true });
-		const discordId = interaction.user.id;
+		const userInfo = {
+			discordId: interaction.user.id,
+		};
 
 		const response = await lark.listRecords(
 			process.env.FEEDBACK_POOL_BASE,
 			process.env.CODES_TABLE,
-			{ filter: 'CurrentValue.[Discord ID] = "' + discordId + '"' }
+			{
+				filter:
+					'CurrentValue.[Discord ID] = "' + userInfo.discordId + '"',
+			}
 		);
 
 		if (!response)
@@ -30,22 +40,102 @@ module.exports = {
 					" button once the code distribution has started to get your code.",
 			});
 
-		const success = await lark.createRecord(
-			process.env.FEEDBACK_POOL_BASE,
-			process.env.CODES_TABLE,
-			{ fields: { "Discord ID": discordId } }
+		const gpuButton1 = new ButtonBuilder()
+			.setCustomId("abovegpu")
+			.setLabel("Above NVIDIA GeForce GTX 1660 Ti 6GB")
+			.setStyle(ButtonStyle.Success);
+
+		const gpuButton2 = new ButtonBuilder()
+			.setCustomId("belowgpu")
+			.setLabel("Below NVIDIA GeForce GTX 1660 Ti 6GB")
+			.setStyle(ButtonStyle.Danger);
+
+		const row1 = new ActionRowBuilder().addComponents(
+			gpuButton1,
+			gpuButton2
 		);
 
-		if (success)
-			return await interaction.editReply({
-				content:
-					"You have been registered! Please use the " +
-					bold("Get Code") +
-					" button once the code distribution has started to get your code.",
-			});
-		else
-			return await interaction.editReply({
-				content: "An error has occured. Please try again later.",
-			});
+		const processorButton1 = new ButtonBuilder()
+			.setCustomId("aboveprocessor")
+			.setLabel("Above Intel Core i7-8700K/AMD Ryzen 5 3600")
+			.setStyle(ButtonStyle.Success);
+
+		const processorButton2 = new ButtonBuilder()
+			.setCustomId("belowprocessor")
+			.setLabel("Below Intel Core i7-8700K/AMD Ryzen 5 3600")
+			.setStyle(ButtonStyle.Danger);
+
+		const row2 = new ActionRowBuilder().addComponents(
+			processorButton1,
+			processorButton2
+		);
+
+		await interaction.editReply({
+			content: "Please select your GPU:",
+			components: [row1],
+		});
+
+		const collector = message.createMessageComponentCollector({
+			componentType: ComponentType.Button,
+			time: 60_000,
+		});
+
+		collector.on("collect", (i) => {
+			if (i.user.id === interaction.user.id) {
+				if (i.customId === "abovegpu") {
+					userInfo.gpu = "Above NVIDIA GeForce GTX 1660 Ti 6GB";
+					interaction.editReply({
+						content: "Please select your processor:",
+						components: [row2],
+					});
+				} else if (i.customId === "belowgpu") {
+					userInfo.gpu = "Below NVIDIA GeForce GTX 1660 Ti 6GB";
+					interaction.editReply({
+						content: "Please select your processor:",
+						components: [row2],
+					});
+				} else if (i.customId === "aboveprocessor") {
+					userInfo.processor =
+						"Above Intel Core i7-8700K/AMD Ryzen 5 3600";
+					sendDataToLark(interaction, userInfo);
+					collector.stop();
+				} else if (i.customId === "belowprocessor") {
+					userInfo.processor =
+						"Below Intel Core i7-8700K/AMD Ryzen 5 3600";
+					sendDataToLark(interaction, userInfo);
+					collector.stop();
+				}
+			}
+		});
+
+		collector.on("end", (collected) => {
+			console.log(`Collected ${collected.size} interactions.`);
+		});
 	},
 };
+
+async function sendDataToLark(interaction, userInfo) {
+	const success = await lark.createRecord(
+		process.env.FEEDBACK_POOL_BASE,
+		process.env.CODES_TABLE,
+		{
+			fields: {
+				"Discord ID": userInfo.discordId,
+				Graphics: userInfo.gpu,
+				Processor: userInfo.processor,
+			},
+		}
+	);
+
+	if (success)
+		return await interaction.editReply({
+			content:
+				"You have been registered! Please use the " +
+				bold("Get Code") +
+				" button once the code distribution has started to get your code.",
+		});
+	else
+		return await interaction.editReply({
+			content: "An error has occured. Please try again later.",
+		});
+}
