ab86392f4bff33f73c1de98b862cda21547fb8a5
Simon <yash1441@yahoo.com>
2024-09-05 13:29:07 +0530

    Deleting guild commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,23 +1,27 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
 require("dotenv").config();
 
 const commands = [];
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".js"));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
+		if ("data" in command && "execute" in command) {
 			commands.push(command.data.toJSON());
 		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
 		}
 	}
 }
@@ -25,16 +29,32 @@ for (const folder of commandFolders) {
 const rest = new REST().setToken(process.env.BOT_TOKEN);
 
 (async () => {
-    try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+	try {
+		console.log(
+			`Started refreshing ${commands.length} application (/) commands.`
+		);
 
-        const data = await rest.put(
-            Routes.applicationCommands(process.env.BOT_ID),
-            { body: commands },
-        );
+		rest.put(
+			Routes.applicationGuildCommands(
+				process.env.BOT_ID,
+				process.env.GUILD_ID
+			),
+			{
+				body: [],
+			}
+		)
+			.then(() => console.log("Successfully deleted all guild commands."))
+			.catch(console.error);
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        console.error(error);
-    }
+		const data = await rest.put(
+			Routes.applicationCommands(process.env.BOT_ID),
+			{ body: commands }
+		);
+
+		console.log(
+			`Successfully reloaded ${data.length} application (/) commands.`
+		);
+	} catch (error) {
+		console.error(error);
+	}
 })();
