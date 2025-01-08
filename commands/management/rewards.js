077cf1df4eebb41f2c7cfca02ca5d6f3907682bf
yash-bst <yash1441@yahoo.com>
2025-01-08 15:49:14 +0700

    Formatting

--- commands/management/rewards.js
+++ commands/management/rewards.js
@@ -2,17 +2,17 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const cronjobs = require("../../cronjobs");
 
 module.exports = {
-	category: "management",
-	data: new SlashCommandBuilder()
-		.setName("rewards")
-		.setDescription("Simulate reward delivery system")
-		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-	async execute(interaction) {
-		await interaction.reply({
-			content: "Started reward delivery system...",
-			ephemeral: true,
-		});
+    category: "management",
+    data: new SlashCommandBuilder()
+        .setName("rewards")
+        .setDescription("Simulate reward delivery system")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        await interaction.reply({
+            content: "Started reward delivery system...",
+            ephemeral: true,
+        });
 
-		cronjobs(interaction.client);
-	},
+        cronjobs(interaction.client);
+    },
 };
