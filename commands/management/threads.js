80cf90e52afb6e874d9638ab86452fa06df48bb8
yash-bst <yash1441@yahoo.com>
2025-01-08 15:43:53 +0700

    Made it subcommand

---
+++ commands/management/threads.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    cooldown: 5,
+    category: "management",
+    data: new SlashCommandBuilder()
+        .setName("threads")
+        .setDescription("Manage threads in the current channel")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("delete")
+                .setDescription("Delete all threads in the current channel")
+        ),
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+
+        if (subcommand === "delete") {
+            const channel = interaction.channel;
+
+            if (!channel.isText()) {
+                return interaction.reply(
+                    "This command can only be used in text channels."
+                );
+            }
+
+            const threads = await channel.threads.fetchActive();
+            const archivedThreads = await channel.threads.fetchArchived();
+
+            const allThreads = threads.threads.concat(archivedThreads.threads);
+
+            for (const thread of allThreads.values()) {
+                await thread.delete();
+            }
+
+            await interaction.reply("All threads have been deleted.");
+        }
+    },
+};
