bbe7a220eca5f9caa9e8a7cb1d9536cf2107ee73
yash-bst <yash1441@yahoo.com>
2025-01-08 15:20:26 +0700

    Added email

--- components/selectmenus/region-select.js
+++ components/selectmenus/region-select.js
@@ -1,90 +1,102 @@
 const {
-	bold,
-	ModalBuilder,
-	TextInputBuilder,
-	ActionRowBuilder,
-	TextInputStyle,
+    bold,
+    ModalBuilder,
+    TextInputBuilder,
+    ActionRowBuilder,
+    TextInputStyle,
 } = require("discord.js");
 const lark = require("../../utils/lark");
 require("dotenv").config();
 
 module.exports = {
-	data: {
-		name: "region-select",
-	},
-	async execute(interaction) {
-		let region = interaction.values[0];
-		const discordId = interaction.user.id;
+    data: {
+        name: "region-select",
+    },
+    async execute(interaction) {
+        let email = "abc@xyz.com";
+        let region = interaction.values[0];
+        const discordId = interaction.user.id;
 
-		if (region === "Others") {
-			const modal = new ModalBuilder()
-				.setCustomId("region-modal")
-				.setTitle("Region");
+        const modal = new ModalBuilder().setCustomId("email-modal");
 
-			const regionInput = new TextInputBuilder()
-				.setCustomId("region-input")
-				.setLabel("Please enter your region")
-				.setStyle(TextInputStyle.Short);
+        const emailInput = new TextInputBuilder()
+            .setCustomId("email-input")
+            .setLabel("Please enter your email")
+            .setStyle(TextInputStyle.Short);
 
-			const row = new ActionRowBuilder().addComponents(regionInput);
-			modal.addComponents(row);
+        const firstRow = new ActionRowBuilder().addComponents(emailInput);
 
-			await interaction.showModal(modal);
+        if (region === "Others") {
+            modal.setTitle("Email & Region");
+            const regionInput = new TextInputBuilder()
+                .setCustomId("region-input")
+                .setLabel("Please enter your region")
+                .setStyle(TextInputStyle.Short);
 
-			const submit = await interaction
-				.awaitModalSubmit({
-					time: 60_000,
-					filter: (i) => i.user.id === interaction.user.id,
-				})
-				.catch((error) => {
-					console.log(error);
-					return null;
-				});
+            const secondRow = new ActionRowBuilder().addComponents(regionInput);
+            modal.addComponents(firstRow, secondRow);
+        } else {
+            modal.setTitle("Email");
+            modal.addComponents(firstRow);
+        }
+        await interaction.showModal(modal);
 
-			if (submit) {
-				region = submit.fields.getTextInputValue("region-input");
-				await submit.reply({
-					content: "Submitted",
-					ephemeral: true,
-				});
-				await submit.deleteReply();
-			} else {
-				return await submit.reply({
-					content: "Timed out.",
-					ephemeral: true,
-				});
-			}
-		}
+        const submit = await interaction
+            .awaitModalSubmit({
+                time: 60_000,
+                filter: (i) => i.user.id === interaction.user.id,
+            })
+            .catch((error) => {
+                console.log(error);
+                return null;
+            });
 
-		await interaction.message.edit({
-			content: "Region selected!" + "\n" + bold(region),
-			components: [],
-		});
+        if (submit) {
+            email = submit.fields.getTextInputValue("email-input");
+            if (region === "Others")
+                region = submit.fields.getTextInputValue("region-input");
+            await submit.reply({
+                content: "Submitted",
+                ephemeral: true,
+            });
+            await submit.deleteReply();
+        } else {
+            return await submit.reply({
+                content: "Timed out.",
+                ephemeral: true,
+            });
+        }
 
-		const records = await lark.listRecords(
-			process.env.COMMUNITY_POOL_BASE,
-			process.env.REWARD_TABLE,
-			{
-				filter: `AND(CurrentValue.[Discord ID] = "${discordId}", CurrentValue.[Status] = "Region Required")`,
-			}
-		);
+        await interaction.message.edit({
+            content: "Region selected!" + "\n" + bold(region),
+            components: [],
+        });
 
-		if (!records.total || !records) return;
+        const records = await lark.listRecords(
+            process.env.COMMUNITY_POOL_BASE,
+            process.env.REWARD_TABLE,
+            {
+                filter: `AND(CurrentValue.[Discord ID] = "${discordId}", CurrentValue.[Status] = "Region Required")`,
+            }
+        );
 
-		await lark.updateRecord(
-			process.env.COMMUNITY_POOL_BASE,
-			process.env.REWARD_TABLE,
-			records.items[0].record_id,
-			{
-				fields: {
-					Region: region,
-					Status: "Reward Required",
-				},
-			}
-		);
+        if (!records.total || !records) return;
 
-		setTimeout(function () {
-			interaction.channel.delete();
-		}, 2_000);
-	},
+        await lark.updateRecord(
+            process.env.COMMUNITY_POOL_BASE,
+            process.env.REWARD_TABLE,
+            records.items[0].record_id,
+            {
+                fields: {
+                    Region: region,
+                    Status: "Reward Required",
+                    Email: email,
+                },
+            }
+        );
+
+        setTimeout(function () {
+            interaction.channel.delete();
+        }, 2_000);
+    },
 };
