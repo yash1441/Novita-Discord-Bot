38bca671e1d20022c88d3a57aa39dfd9843b17bf
Simon <yash1441@yahoo.com>
2025-02-19 12:32:51 +0530

    Added pickup line votes

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,29 +1,73 @@
-const { Events } = require('discord.js');
-const lark = require('../utils/lark.js');
-require('dotenv').config();
+const { Events } = require("discord.js");
+const lark = require("../utils/lark.js");
+require("dotenv").config();
 
 module.exports = {
-    name: Events.MessageReactionAdd,
-    async execute(reaction, user) {
-        if (reaction.partial) {
-            try {
-                await reaction.fetch();
-            } catch (error) {
-                return console.error(error);
-            }
-        }
+	name: Events.MessageReactionAdd,
+	async execute(reaction, user) {
+		if (reaction.partial) {
+			try {
+				await reaction.fetch();
+			} catch (error) {
+				return console.error(error);
+			}
+		}
 
-        if (user.id == process.env.BOT_ID || (reaction.emoji.name != '✅' && reaction.emoji.name != '❌') || (reaction.message.channel.parentId != process.env.VOTE_SUGGESTION_ID && reaction.message.channel.parentId != process.env.VOTE_SUGGESTION_ID_JP)) return;
+		const isBot = user.id == process.env.BOT_ID;
+		const isVoteSuggestion = [
+			process.env.VOTE_SUGGESTION_ID,
+			process.env.VOTE_SUGGESTION_ID_JP,
+		].includes(reaction.message.channel.parentId);
+		const isVoteLine =
+			reaction.message.channel.parentId == process.env.VOTE_LINE_ID;
 
-        const acceptCount = reaction.message.reactions.cache.get('✅').count - 1;
-        const rejectCount = reaction.message.reactions.cache.get('❌').count - 1;
+		if (isBot || (!isVoteSuggestion && !isVoteLine)) return;
 
-        const response = await lark.listRecords(process.env.FEEDBACK_POOL_BASE, process.env.SUGGESTIONS_TABLE, { filter: 'AND(CurrentValue.[Suggestion Title] = "' + reaction.message.channel.name + '", CurrentValue.[Discord Name] = "' + reaction.message.embeds[0].data.author.name + '")' });
+		let acceptCount, rejectCount, base, table, filter;
 
-        if (!response) return console.warn('Could not fetch Suggestion Title: ' + reaction.message.channel.name);
+		if (isVoteSuggestion) {
+			if (!["✅", "❌"].includes(reaction.emoji.name)) return;
+			acceptCount = reaction.message.reactions.cache.get("✅").count - 1;
+			rejectCount = reaction.message.reactions.cache.get("❌").count - 1;
+			base = process.env.FEEDBACK_POOL_BASE;
+			table = process.env.SUGGESTIONS_TABLE;
+			filter = `AND(CurrentValue.[Suggestion Title] = "${reaction.message.channel.name}", CurrentValue.[Discord Name] = "${reaction.message.embeds[0].data.author.name}")`;
+		} else if (isVoteLine) {
+			if (!["❤️", "💔"].includes(reaction.emoji.name)) return;
+			acceptCount = reaction.message.reactions.cache.get("❤️").count - 1;
+			rejectCount = reaction.message.reactions.cache.get("💔").count - 1;
+			base = process.env.COMMUNITY_POOL_BASE;
+			table = process.env.LINE_TABLE;
+			filter = `AND(CurrentValue.[Title] = "${reaction.message.channel.name}", CurrentValue.[Discord Username] = "${reaction.message.embeds[0].data.author.name}")`;
+		}
 
-        const success = await lark.updateRecord(process.env.FEEDBACK_POOL_BASE, process.env.SUGGESTIONS_TABLE, response.items[0].record_id, { fields: { '✅': acceptCount, '❌': rejectCount } });
+		const response = await lark.listRecords(base, table, { filter });
 
-        if (!success) return console.warn('Could not add ' + reaction.emoji.name + ' to ' + reaction.message.id);
-    }
+		if (!response) {
+			return console.warn(
+				`Could not fetch record for: ${reaction.message.channel.name}`
+			);
+		}
+
+		const success = await lark.updateRecord(
+			base,
+			table,
+			response.items[0].record_id,
+			{
+				fields: {
+					[reaction.emoji.name]:
+						reaction.emoji.name === "✅" ||
+						reaction.emoji.name === "❤️"
+							? acceptCount
+							: rejectCount,
+				},
+			}
+		);
+
+		if (!success) {
+			return console.warn(
+				`Could not add ${reaction.emoji.name} to ${reaction.message.id}`
+			);
+		}
+	},
 };
