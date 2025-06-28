d65fa4554d1b6577df97ec6852aed05bce720db9
Simon <yash1441@yahoo.com>
2025-06-28 10:17:12 +0530

    Creates LFG thread in forum channel
    Implements the creation of a new "Looking For Group" (LFG) thread within a designated forum channel.
    
    The thread includes an embed with details such as gameplay mode, region, team description, language, and time.
    It also applies appropriate tags to the thread and updates the user's modal reply with a link to the newly created thread.

--- components/selectmenus/lfg-gameplay-mode.js
+++ components/selectmenus/lfg-gameplay-mode.js
@@ -4,6 +4,8 @@ const {
 	ActionRowBuilder,
 	TextInputStyle,
 	MessageFlags,
+	messageLink,
+	EmbedBuilder,
 } = require("discord.js");
 const lark = require("../../utils/lark");
 require("dotenv").config();
@@ -114,8 +116,50 @@ module.exports = {
 			? (data.region = modalReply.fields.getTextInputValue("region-input"))
 			: (data.region = "JP");
 
+		const forumChannelId =
+			serverId === process.env.GUILD_ID
+				? process.env.LFG_CHANNEL
+				: process.env.LFG_CHANNEL_JP;
+		const forumChannel = await interaction.client.channels.fetch(
+			forumChannelId
+		);
+		const availableTags = forumChannel.availableTags;
+		for (const tag of availableTags) {
+			if (tag.name == data.gameplayMode) {
+				data.tagId = tag.id;
+			}
+		}
+
+		const lfgEmbed = new EmbedBuilder()
+			.setColor(process.env.EMBED_COLOR)
+			.setTitle(
+				serverId === process.env.GUILD_ID
+					? "Looking for Teammates"
+					: "チームメンバー募集"
+			)
+			.setDescription(
+				`**Gameplay Mode:** ${data.gameplayMode}\n**Region:** ${data.region}\n**Team Description:** ${data.teamDescription}\n**Language:** ${data.language}\n**Time:** ${data.time}`
+			)
+			.setFooter({
+				text: `Posted by ${interaction.user.username}`,
+				iconURL: interaction.user.displayAvatarURL(),
+			});
+
+		const forumThread = await forumChannel.threads.create({
+			name: `${data.gameplayMode} | ${interaction.user.username}`,
+			reason: interaction.user.username + " created a LFG thread",
+			message: {
+				embeds: [lfgEmbed],
+			},
+			appliedTags: [data.tagId],
+		});
+
+		const threadMessage = await forumThread.fetchStarterMessage();
+
 		await modalReply.editReply({
-			content: `**Gameplay Mode:** ${data.gameplayMode}\n**Region:** ${data.region}\n**Team Description:** ${data.teamDescription}\n**Language:** ${data.language}\n**Time:** ${data.time}`,
+			content:
+				"Your LFG thread has been created: " +
+				messageLink(threadMessage.channel.id, threadMessage.id),
 			components: [],
 		});
 	},
