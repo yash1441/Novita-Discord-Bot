84c5d53c25b012b6627d637281734262a0d2b629
Simon <yash1441@yahoo.com>
2025-07-29 20:03:08 +0530

    Adds reward system start log
    Adds logging to a designated channel when the reward system is started.
    
    This allows administrators to track when the reward system is manually
    triggered via the command.

--- commands/management/rewards.js
+++ commands/management/rewards.js
@@ -1,22 +1,34 @@
 const {
-    SlashCommandBuilder,
-    PermissionFlagsBits,
-    MessageFlags,
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	MessageFlags,
 } = require("discord.js");
 const cronjobs = require("../../cronjobs");
 
 module.exports = {
-    category: "management",
-    data: new SlashCommandBuilder()
-        .setName("rewards")
-        .setDescription("Simulate reward delivery system")
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-    async execute(interaction) {
-        await interaction.reply({
-            content: "Started reward delivery system...",
-            flags: MessageFlags.Ephemeral,
-        });
+	category: "management",
+	data: new SlashCommandBuilder()
+		.setName("rewards")
+		.setDescription("Simulate reward delivery system")
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	async execute(interaction) {
+		await interaction.reply({
+			content: "Started reward delivery system...",
+			flags: MessageFlags.Ephemeral,
+		});
 
-        cronjobs(interaction.client);
-    },
+		const channel = await client.channels.fetch(process.env.LOG_CHANNEL);
+
+		if (!channel) return;
+
+		await channel.send({
+			content:
+				interaction.user.username +
+				" (" +
+				interaction.user.id +
+				") has started the reward delivery system.",
+		});
+
+		cronjobs(interaction.client);
+	},
 };
