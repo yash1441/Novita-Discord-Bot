df3bfa6142898b80bdf6e0725934f5248b1ce54b
Simon <yash1441@yahoo.com>
2026-09-22 16:40:21 +0530

    Adds Message Content Gateway Intent
    This intent is necessary for the bot to read and process the content of messages, enabling core functionality that relies on user input.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.GuildMessageReactions,
 		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent,
 	],
 	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
